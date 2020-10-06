b7586d2d9383c17a7dc889fa002469f472980d4c
chengxxi <heeya121z@naver.com>
2020-10-07 00:00:38 +0900

    chore: admin change list customizing

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Choice, Question
 
 # Register your models here.
-class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
     model = Choice
     extra = 3
 
@@ -13,5 +13,13 @@ class QuestionAdmin(admin.ModelAdmin):
         ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
     ]
     inlines = [ChoiceInline]
+
+    # 개별 필드를 표시하기 위한 튜플 (객체의 변경 목록 페이지에서 열로 표시됨)
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # Question 변경 목록 페이지에 개선점 추가
+    list_filter = ['pub_date']
+    # 검색 기능 추가
+    search_fields = ['question_text']
+    
 
 admin.site.register(Question, QuestionAdmin)
